1969b20935a2f9f9cd445bb8fc4967e83b0f6043
Robin Nowak <nowak.robin33@gmail.com>
2021-01-16 15:00:09 +0100

    chore: refactored algorithm - ToDo: readFile

--- Python/Door1/door1.py
+++ Python/Door1/door1.py
@@ -1,32 +1,19 @@
 puzzleInput = [1722, 1010, 300, 1337, 200, 299, 683]
-i = 0
-desiredNumber = 2020
-summands = []
-
-def checkForDesiredNumber(n1, n2):
-    if n1 + n2 == desiredNumber:
-        print("Got it!")
-        summands.append(n1)
-        summands.append(n2)
-        print(summands)
-        return summands
-
-for number1 in puzzleInput:
-    for number2 in puzzleInput[i + 1:]:
-        checkForDesiredNumber(number1, number2)
-    i = i + 1
-    print(i)
 
 
+def searchNumbers(): 
+    i = 0
+    desiredNumber = 2020
+    print("Looking for your numbers... (╯°□°）╯︵ ┻━┻")
 
+    for number1 in puzzleInput:
+        for number2 in puzzleInput[i + 1:]:
+            if number1 + number2 == desiredNumber:
+                print("Got it! ┬─┬ ノ( ゜-゜ノ)")
+                print("/", number1, "&", number2, "/")
+                print("Now multiplying the numbers...")
+                print("Here is your final number:", number1*number2)
+                return [number1*number2]
+        i += 1
 
-"""
-for number1 in puzzleInput:
-    for number2 in puzzleInput[i + 1:]:
-        if number1 + number2 == desiredNumber:
-            print("Got it!")
-            summands.append(number1)
-            summands.append(number2)
-            break
-    i = i + 1
-"""
+searchNumbers()
